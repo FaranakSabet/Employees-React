4489be59133889d96c88ab11de35e13a5b1157e3
faranak Sabet <faranaksabet@yahoo.com>
2020-11-05 13:09:34 -0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,3 +6,15 @@ import Typography from "@material-ui/core/Typography"
 import Container from "@material-ui/core/Container"
 import Paper from "@material-ui/core/Paper"
 import { DataGrid } from "@material-ui/data-grid"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
